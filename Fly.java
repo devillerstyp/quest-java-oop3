d201b7867495ee39bb6e814a65f826e53c19e8ea
devillerstyp <53711739+devillerstyp@users.noreply.github.com>
2020-05-22 21:19:50 +0200

    Update Fly.java

--- Fly.java
+++ Fly.java
@@ -1,5 +1,16 @@
 package questJavaoop3;
 public  class Fly {
 
+     
+    int ascend(int meters);
+
+    int descend(int meters);
     
+    void takeOff();
+
+    default void glide() {
+    System.out.println("It glides into the air.");
+    }
+    void land();
+
 }  
